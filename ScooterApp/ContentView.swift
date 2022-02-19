ab2b75d89ed474d07101f1c69f23b0d921c8270d
aeratal <ali_ugur.eratalar@daimler.com>
2022-02-19 08:09:10 +0300

    Initial Commit

---
+++ ScooterApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ScooterApp
+//
+//  Created by Eratalar, Ali Ugur (401) on 19.02.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ScooterApp/ScooterAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScooterAppApp.swift
+//  ScooterApp
+//
+//  Created by Eratalar, Ali Ugur (401) on 19.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct ScooterAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
